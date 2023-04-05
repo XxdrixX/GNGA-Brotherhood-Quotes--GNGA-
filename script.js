7490bc028cf7d9904280bff0c671024cbf63182d
XxdrixX <daveranola351@gmail.com>
2023-04-05 17:27:58 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+(function() {
+  $(document ).ready(function() {
+      getVerse();
+  });
+
+
+      $.ajax({
+        url: "https://labs.bible.org/api/?passage=random&type=json&callback=myCallback", 
+        crossDomain: true,
+        dataType: 'jsonp',
+        success: function(result){
+           $("#newQuote")
+             .html('<strong>'+
+                   result[0].bookname+
+                   ' ' + result[0].chapter +
+                   ':' + result[0].verse +
+                   '</strong> ' +
+                   result[0].text);
+
+          $("#spinner").hide();
+        }
+      });
+  }
+)();
